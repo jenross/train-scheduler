37bd84fbb255c6905ffcb033c4cdfbfd8294a236
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-07-14 12:17:04 -0400

    began adding basic html structure with bootstrap components

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBHF1sOJoy6LVFJvvYPDDW8239VpLp_bdE",
+    authDomain: "train-time-90e3d.firebaseapp.com",
+    databaseURL: "https://train-time-90e3d.firebaseio.com",
+    projectId: "train-time-90e3d",
+    storageBucket: "",
+    messagingSenderId: "555293648276",
+    appId: "1:555293648276:web:308a460bd7e2b5f6"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
